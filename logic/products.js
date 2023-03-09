8246473cd5162f4ea26edc4c3ad0d6d8857bb199
amr.kakashi <93amr.kakashi@gmail.com>
2023-03-09 20:10:13 +0200

    edit products

--- logic/products.js
+++ logic/products.js
@@ -1,44 +1,51 @@
 const products = [
   {
-    id: 1,
+    id:1,
     product_name: "Audio Cable",
     product_price: "9",
-    product_image: "../images/cable.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F3502c776-a794-44c9-a45d-a22e7eaa878f?alt=media&token=bdb65069-41e4-417b-b468-982c964c1726",
     added_to_cart: false,
   },
   {
-    id: 2,
+    id:2,
     product_name: "Smart Watch",
     product_price: "21.9",
-    product_image: "../images/smart-watch.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F3a6cdd22-7d40-4af4-8a96-61e4a1783702?alt=media&token=fe94701e-04c2-411f-abfe-65d9c6c34a3d",
     added_to_cart: false,
   },
   {
-    id: 3,
+    id:3,
     product_name: "Headphones",
     product_price: "7.5",
-    product_image: "../images/head-ohones.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2Fb2c1d06c-8bad-4b4a-a575-fe4b0827259f?alt=media&token=5c79d96d-fd1d-48ea-9983-79d2e22c222a",
     added_to_cart: false,
   },
   {
-    id: 4,
+    id:4,
     product_name: "Remote Control",
     product_price: "13",
-    product_image: "../images/remote.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F865a2589-baa1-4747-b842-9a782a46f611?alt=media&token=9439b86a-a9bd-4e2a-9aa9-a982ac60bc96",
     added_to_cart: false,
   },
   {
-    id: 5,
+    id:5,
     product_name: "Reading Tablet",
     product_price: "46",
-    product_image: "../images/reading.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F6311d090-e723-421f-921c-6489b2a486a2?alt=media&token=1e3897f4-63e3-4b20-b618-cdd0162e33b3",
     added_to_cart: false,
   },
   {
-    id: 6,
+    id:6,
     product_name: "HD TV",
     product_price: "975",
-    product_image: "../images/screen.png",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2Fdf74b258-7a62-4555-a71e-3cd540e582ba?alt=media&token=29570444-89a7-45d8-b8ee-6282821b30a8",
     added_to_cart: false,
   },
 ];
+
