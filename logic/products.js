203077b4afb51cf5c3e935e4c3d920f151fcca2d
amr.kakashi <93amr.kakashi@gmail.com>
2023-03-09 20:07:05 +0200

    test

--- logic/products.js
+++ logic/products.js
@@ -1,51 +1,44 @@
 const products = [
   {
-    id:1,
+    id: 1,
     product_name: "Audio Cable",
     product_price: "9",
-    product_image:
-      "../images/cable.png",
+    product_image: "../images/cable.png",
     added_to_cart: false,
   },
   {
-    id:2,
+    id: 2,
     product_name: "Smart Watch",
     product_price: "21.9",
-    product_image:
-      "../images/smart-watch.png",
+    product_image: "../images/smart-watch.png",
     added_to_cart: false,
   },
   {
-    id:3,
+    id: 3,
     product_name: "Headphones",
     product_price: "7.5",
-    product_image:
-      "../images/head-ohones.png",
+    product_image: "../images/head-ohones.png",
     added_to_cart: false,
   },
   {
-    id:4,
+    id: 4,
     product_name: "Remote Control",
     product_price: "13",
-    product_image:
-      "../images/remote.png",
+    product_image: "../images/remote.png",
     added_to_cart: false,
   },
   {
-    id:5,
+    id: 5,
     product_name: "Reading Tablet",
     product_price: "46",
-    product_image:
-      "../images/reading.png",
+    product_image: "../images/reading.png",
     added_to_cart: false,
   },
   {
-    id:6,
+    id: 6,
     product_name: "HD TV",
     product_price: "975",
-    product_image:
-      "../images/screen.png",
+    product_image: "../images/screen.png",
     added_to_cart: false,
   },
 ];
-
