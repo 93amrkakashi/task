4fd047dfa1ff0cf361287520178ca875b17e5f2e
93amr.kakashi <93amr.kakashi@gmail.com>
2023-03-25 20:44:06 +0700

    productss

---
+++ logic/products.ts
@@ -0,0 +1,52 @@
+const productss = [
+  {
+    id:1,
+    product_name: "Audio Cable",
+    product_price: "9",
+    product_image:
+      "../images/cable.png",
+    added_to_cart: false,
+  },
+  {
+    id:2,
+    product_name: "Smart Watch",
+    product_price: "21.9",
+    product_image:
+      "../images/head-ohones.png",
+    added_to_cart: false,
+  },
+  
+  {
+    id:3,
+    product_name: "Headphones",
+    product_price: "7.5",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2Fb2c1d06c-8bad-4b4a-a575-fe4b0827259f?alt=media&token=5c79d96d-fd1d-48ea-9983-79d2e22c222a",
+    added_to_cart: false,
+  },
+  {
+    id:4,
+    product_name: "Remote Control",
+    product_price: "13",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F865a2589-baa1-4747-b842-9a782a46f611?alt=media&token=9439b86a-a9bd-4e2a-9aa9-a982ac60bc96",
+    added_to_cart: false,
+  },
+  {
+    id:5,
+    product_name: "Reading Tablet",
+    product_price: "46",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2F6311d090-e723-421f-921c-6489b2a486a2?alt=media&token=1e3897f4-63e3-4b20-b618-cdd0162e33b3",
+    added_to_cart: false,
+  },
+  {
+    id:6,
+    product_name: "HD TV",
+    product_price: "975",
+    product_image:
+      "https://firebasestorage.googleapis.com/v0/b/shopping-app-cd303.appspot.com/o/products%2Fdf74b258-7a62-4555-a71e-3cd540e582ba?alt=media&token=29570444-89a7-45d8-b8ee-6282821b30a8",
+    added_to_cart: false,
+  },
+];
+
